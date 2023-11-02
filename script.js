2851499c9dd8d5fc341e6d3913e02f555dd970fb
brandonom <brandonomyers1@gmail.com>
2023-11-02 12:41:33 -0400

    function should apply classes based on time to blocks

--- script.js
+++ script.js
@@ -16,4 +16,22 @@ $(document).ready(function() {
         
         localStorage.setItem(hour, text);
       });
+});
+
+
+
+function updateHourBlocks(){
+    const currenthour = now.hour();
+
+    $(".time-block").each(function(){
+        const blockHour = parseInt($(this).attr('id').split("-")[1]);
+
+        if (blockHour < currenthour){
+            $(this).removeClass('present future').addClass('past');
+        } else if (blockHour === currentHour) {
+            $(this).removeClass("past future").addClass("present");
+          } else {
+            $(this).removeClass("past present").addClass("future");
+          }
+    });
 }

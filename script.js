bf1ff9659aacdc6a6433df6d018f638b1099a080
brandonom <brandonomyers1@gmail.com>
2023-11-02 12:17:05 -0400

    added function that should allow user to save

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 $(document).ready(function() {
     const now = dayjs();
+
+    function showDate() {
+        $("#currentDay").text(now.format('dddd, MMMM D, YYYY')); 
+      }
+    
+      showDate(); 
+    
+     
+      $(".saveBtn").on("click", function() {
+       
+        const hour = $(this).parent().attr("id");
+        const text = $(this).siblings("textarea").val();
+    
+        
+        localStorage.setItem(hour, text);
+      });
+}
